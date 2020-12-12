b0d6f6efed74e9a0b85abea68af67cd2fabbf8d1
Mayur Wani <mayurw0819@gmail.com>
2020-12-11 23:22:48 -0500

    Add exisiting files

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.export = {
-  mongoURI:
-    'mongodb+srv://mayur:mayur@cluster0-svqdg.mongodb.net/test?retryWrites=true&w=majority',
-};
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// connect database
+connectDB();
+
+// init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
 // single endpoint
 app.get('/', (req, res) => res.send('API running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const port = process.env.PORT || 5000;
 
